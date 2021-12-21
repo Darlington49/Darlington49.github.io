a1c943a212a23cf1cd424458c3dc36d641073c57
Youcef Ali <youcef.ali@smartest.dz>
2021-12-21 15:26:25 +0100

    add ble

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ var button = document.getElementById("myButton");
 
 button.addEventListener("pointerup", function () {
     document.getElementById("demo").innerHTML = "Hello World";
-    navigator.bluetooth.requestDevice({ 
+    navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
-            //filters: [{ services: ['battery_service'] }] 
-        }        )
-        .then(device => { /* … */ })
+        //filters: [{ services: ['battery_service'] }] 
+    })
+        .then(device => {
+            // Human-readable name of the device.
+            console.log(device.name);
+
+            // Attempts to connect to remote GATT Server.
+            return device.gatt.connect();
+        })
+        .then(server => { /* … */ })
         .catch(error => { console.error(error); });
 });
