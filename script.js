e049e74eeb78bb515c0d6606de27a6c2edeaf4b9
Youcef Ali <youcef.ali@smartest.dz>
2021-12-21 15:08:35 +0100

    add ble

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hi from script js")
+var button = document.getElementById("myButton");
+// button.addEventListener('pointerup', function (event) {
+//     // Call navigator.bluetooth.requestDevice
+//     console.log(event)
+// });
+
+button.addEventListener("pointerup", function () {
+    document.getElementById("demo").innerHTML = "Hello World";
+    navigator.bluetooth.requestDevice()
+});
