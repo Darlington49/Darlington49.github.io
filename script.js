7a8e11a952d8986ee0c96fe0f7c087d14a4d57eb
Youcef Ali <youcef.ali@smartest.dz>
2021-12-21 15:22:06 +0100

    add ble

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ var button = document.getElementById("myButton");
 
 button.addEventListener("pointerup", function () {
     document.getElementById("demo").innerHTML = "Hello World";
-    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+    navigator.bluetooth.requestDevice(
+        // { filters: [{ services: ['battery_service'] }] }
+        )
         .then(device => { /* … */ })
         .catch(error => { console.error(error); });
 });
